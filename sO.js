c635d2adb8e2bac528f1652d7088e09354f578cc
And111rey <Samu111ray@gmail.com>
2018-09-11 02:32:29 +0300

    task about fitsr letter Upp

---
+++ sO.js
@@ -0,0 +1,10 @@
+
+
+
+
+function ucFirst(str) {	
+	var newString = str[0].toUpperCase() + str.slice(1);
+	alert(newString);
+}
+ucFirst(prompt("Введите слово, первую букву которого нужно сдклать заглавной"));
+
